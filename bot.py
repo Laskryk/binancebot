977ec8e4165aee53b2efdcbff8e5e21f9249977f
Laskryk <laskryk_0125@yahoo.com>
2025-03-24 20:14:41 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import ccxt
 
 # Introdu manual cheile API aici
-api_key = "CHEIA_TA_API"w7NhaaVholURZ5I69hmMJznOHJKxQpNIKsIc8Yb0uIVtZLDcQan5m9Rm4TbhwMg
-api_secret = "SECRETUL_TĂU_API"YmIzU0bsUo8bPF9lpz3WulMMqePGAn7U3IPSz7UawN92tX2fsKsePYiiUIQE8iz7
+api_key = "CHEIA_TA_API"w7NhaaVholURZ5I69hmMJznOHJKxQpNIKsIc8Yb0uIVtZLDcQan5m9Rm4TbhwMg"
+api_secret = "SECRETUL_TĂU_API"YmIzU0bsUo8bPF9lpz3WulMMqePGAn7U3IPSz7UawN92tX2fsKsePYiiUIQE8iz7"
 
 # Conectează-te la Binance
 exchange = ccxt.binance({
